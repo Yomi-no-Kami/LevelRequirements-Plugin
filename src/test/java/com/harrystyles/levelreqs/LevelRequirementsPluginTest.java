45d1cf47c7b16264c35fa3fff22b7d416cb7289d
VerZik <juddy_beckham@hotmail.com>
2026-09-17 18:46:44 -0400

    Fixed 1 instance of refactoring the project
    Fixed an error that was associated with the example plugin

--- src/test/java/com/harrystyles/levelreqs/LevelRequirementsPluginTest.java
+++ src/test/java/com/harrystyles/levelreqs/LevelRequirementsPluginTest.java
@@ -7,7 +7,7 @@ public class LevelRequirementsPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(LevelRequirementsPlugin.class);
 		RuneLite.main(args);
 	}
 }
